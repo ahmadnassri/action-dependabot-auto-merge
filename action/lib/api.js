1da24bb86c28741f890678885fbbd00c8246779d
Ahmad Nassri <ahmad@ahmadnassri.com>
2020-08-25 15:20:29 -0400

    refactor(approval): pR approvals can be done with one api call

--- action/lib/api.js
+++ action/lib/api.js
@@ -1,14 +1,8 @@
 export async function approve (octokit, repo, { number }) {
-  const { data: { id } } = await octokit.pulls.createReview({
-    ...repo,
-    pull_number: number
-  })
-
-  await octokit.pulls.submitReview({
+  await octokit.pulls.createReview({
     ...repo,
     pull_number: number,
-    event: 'APPROVE',
-    review_id: id
+    event: 'APPROVE'
   })
 }
 
