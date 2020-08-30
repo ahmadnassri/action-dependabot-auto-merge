0c42673f1855e125a9e3a78bd81be6f8bbd1c9e3
Dominic Griesel <d.griesel@gmx.net>
2020-08-30 14:18:47 +0200

    skip everything but debug code

--- action/index.js
+++ action/index.js
@@ -1,36 +1,36 @@
-// internals
-import { inspect } from 'util'
+// // internals
+// import { inspect } from 'util'
 
-// packages
-import core from '@actions/core'
+// // packages
+// import core from '@actions/core'
 
-// modules
-import main from './lib/index.js'
+// // modules
+// import main from './lib/index.js'
 
-// parse inputs
-const inputs = {
-  token: core.getInput('github-token', { required: true }),
-  target: core.getInput('target', { required: true }),
-  command: core.getInput('command', { required: false }),
-  approve: core.getInput('approve', { required: false })
-}
+// // parse inputs
+// const inputs = {
+//   token: core.getInput('github-token', { required: true }),
+//   target: core.getInput('target', { required: true }),
+//   command: core.getInput('command', { required: false }),
+//   approve: core.getInput('approve', { required: false })
+// }
 
-// error handler
-function errorHandler ({ message, stack, request }) {
-  core.error(`${message}\n${stack}`)
+// // error handler
+// function errorHandler ({ message, stack, request }) {
+//   core.error(`${message}\n${stack}`)
 
-  // debugging for API calls
-  if (request) {
-    const { method, url, body, headers } = request
-    core.debug(`${method} ${url}\n\n${inspect(headers)}\n\n${inspect(body)}`)
-  }
+//   // debugging for API calls
+//   if (request) {
+//     const { method, url, body, headers } = request
+//     core.debug(`${method} ${url}\n\n${inspect(headers)}\n\n${inspect(body)}`)
+//   }
 
-  process.exit(1)
-}
+//   process.exit(1)
+// }
 
-// catch errors and exit
-process.on('unhandledRejection', errorHandler)
-process.on('uncaughtException', errorHandler)
+// // catch errors and exit
+// process.on('unhandledRejection', errorHandler)
+// process.on('uncaughtException', errorHandler)
 
 import { execSync } from "child_process";
 
